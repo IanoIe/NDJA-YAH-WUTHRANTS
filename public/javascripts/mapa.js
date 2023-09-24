c7a3b5635dd14c2cc8df34848cfed7f00c1f2e9c
AC_Marvel <gil_1_10@hotmail.com>
2023-09-24 13:23:50 +0200

    Marcador da localização do setor de região no mapa do região de Bissau

--- public/javascripts/mapa.js
+++ public/javascripts/mapa.js
@@ -1,49 +1,13 @@
-var routes = [];
-var map;
-var marker;
 
 
-/**Esta função permite carregar o mapa e centraliza-lo nas coordenas que estao abaixo, neste caso coordenadas de Madrid */
-function carregarMapa(idElement){
-  var mapa = L.map(idElement)
-  var attribution = '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors';
-  var tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  var tiles = L.tileLayer(tileUrl, { attribution });
-  tiles.addTo(mapa);
-  /**Cordenadas da localização de Ondame */
-  mapa.setView([11.93221091136645, -15.801588950107647], 11).addTo(mapa);
-  
-  L.marker([11.93221091136645, -15.801588950107647]).addTo(mapa)
-  .bindPoup('A pretty CSS popup.<br> Easily customizable.')
-  .openPoup();
-  return mapa
-}
+var map = L.map('map').setView([11.895015, -15.850439], 11);
 
-/**Função que permite pesquisar a localização atraves da barra de pesquisa e mostra na tela
-Recebe a string do utilizador e vai sugerindo e quando clicando da as coords e nome da zona de localização */
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-async function geolocalizacao(event){
-  var localizacao = event.target.value;
-
-  if (localizacao.trim() !== ""){
-    var resultados = await $.ajax({
-      url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${localizacao}.json?country=pt&limit=5&proximity=ip&types=place%2Cpostcode%2Caddress&language=pt&access_token=pk.eyJ1IjoiZ2FzZGYyMiIsImEiOiJjbDFvN3RkancwMnVrM2RrYnNjZGJhYTduIn0.IuFv3hseMpUHRZY-TnhbDw`,
-      method: "get",
-      dataType: "json",
-    });
-
-    var html = "";
-
-    for (var resultado of resultados.features){
-      html += ` <button onclick="selecionarLocalizacao(${resultado.center[0]}, ${resultado.center[1]})" type="button">${resultado.place_name_pt}</button>`;
-    }
-    document.querySelector("#resultados").innerHTML = html;
-  } else {
-    document.querySelector("#resultados").innerHTML = "";
-  }
-}
-
-
-mapa = carregarMapa('map');
+L.marker([11.895015, -15.850439]).addTo(map)
+    .bindPopup('Sector de Quinhamel.<br>')
+    .openPopup();
 
 
